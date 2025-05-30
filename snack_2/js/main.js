81b38d93139d06d048919d1e1224ae2259e6100e
francescoboschelle <francibossch@gmail.com>
2025-05-30 11:39:06 +0200

    Refactor string comparison logic to ensure user input is validated before comparison; added alerts for empty input cases.

--- snack_2/js/main.js
+++ snack_2/js/main.js
@@ -1,12 +1,29 @@
 const string1 = prompt("Inserisci la prima parola");
-const string2 = prompt("Inserisci la seconda parola");
 
-if (string1.length === string2.length) {
-  alert("Entrambe le parole sono lunghe uguali!");
+if (string1) {
+  const string2 = prompt("Inserisci la seconda parola");
+
+  if (string2) {
+    if (string1.length === string2.length) {
+      alert("Entrambe le parole sono lunghe uguali!");
+    } else {
+      alert(string1.length < string2.length ? string1 : string2);
+      alert(string1.length > string2.length ? string1 : string2);
+    }
+  } else {
+    alert("Devi inserire almeno una parola!");
+  }
 } else {
-  const shortestString = string1.length < string2.length ? string1 : string2;
-  const longestString = string1.length > string2.length ? string1 : string2;
-
-  alert(shortestString);
-  alert(longestString);
+  alert("Devi inserire almeno una parola!");
 }
+
+// ALTERNATIVA
+/* if (string1.length === string2.length) {
+  alert("Entrambe le parole sono lunghe uguali!");
+} else if (string1.length > string2.length) {
+  alert(string2);
+  alert(string1);
+} else if (string1.length < string2.length) {
+  alert(string1);
+  alert(string2);
+} */
